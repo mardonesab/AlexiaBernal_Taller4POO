92d63d313668b1dd2e67bfcab9d6e79b96e478aa
Alexia <alexia.bernal@alumnos.ucn.cl>
2025-12-10 12:11:27 -0300

    Creación de la clase UsuariosFactory que nos permite crear usuarios segun el rol de cada uno, con esta clase tambien evitamos de escribir este codigo en en otras clases del proyecto

---
+++ AlexiaBernal_Taller4POO/src/logica/UsuariosFactory.java
@@ -0,0 +1,24 @@
+package logica;
+
+import dominio.Administrador;
+import dominio.Coordinador;
+import dominio.Estudiante;
+
+public class UsuariosFactory 
+{
+	public Administrador crearAdministrador(String nombre, String contraseña)
+	{
+		return new Administrador(nombre, contraseña);
+	}
+	
+	public Coordinador crearCoordinador(String nombre, String contraseña, String area)
+	{
+		return new Coordinador(nombre, contraseña, area);
+	}
+	
+	public Estudiante crearEstudiante(String nombre, String contraseña, String rut, String nombreCompleto, String carrera,
+			int semestre, String correo)
+	{
+		return new Estudiante(nombre, contraseña, rut, nombreCompleto, carrera, semestre, correo);
+	}
+}
