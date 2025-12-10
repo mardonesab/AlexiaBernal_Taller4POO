489a50d59aac136fb7594f93e30ff7d4d0adb917
Alexia <alexia.bernal@alumnos.ucn.cl>
2025-12-10 19:25:01 -0300

    -Creación de botones en la parte superior de la ventana de MenuAdministrador y cambio de tamaño de la misma ventada.
    No hay funciones asociadas a los botones por ahora

--- AlexiaBernal_Taller4POO/src/presentacion/MenuAdministrador.java
+++ AlexiaBernal_Taller4POO/src/presentacion/MenuAdministrador.java
@@ -1,13 +1,36 @@
 package presentacion;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MenuAdministrador extends JFrame 
 {
 	public MenuAdministrador()
 	{
 		setTitle("Menu Administrador");
-		setSize(300, 200);
+		setSize(600, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		
+		JPanel panelBotones = new JPanel();
+		panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.LINE_AXIS));
+		
+		JButton botonCrear = new JButton("Crear usuario");
+		JButton botonModificar = new JButton("Modificar usuario");
+		JButton botonEliminar = new JButton("Eliminar usuario");
+		JButton botonRestablecer = new JButton("Restablecer contraseña");
+		
+		panelBotones.add(botonCrear);
+		panelBotones.add(botonModificar);
+		panelBotones.add(botonEliminar);
+		panelBotones.add(botonRestablecer);
+		
+		add(panelBotones, BorderLayout.NORTH);
+		
+		
+		
 	}
 }
