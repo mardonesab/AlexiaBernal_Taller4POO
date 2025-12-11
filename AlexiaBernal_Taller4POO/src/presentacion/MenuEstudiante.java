3b34530e7042155979ea7bcee6b8f267d22d857e
Alexia <alexia.bernal@alumnos.ucn.cl>
2025-12-10 21:18:11 -0300

    -Creación de botones en la parte superior de la ventana de MenuEstudiante y cambio de tamaño de la misma ventana.
    No hay funciones asociadas a los botones por ahora

--- AlexiaBernal_Taller4POO/src/presentacion/MenuEstudiante.java
+++ AlexiaBernal_Taller4POO/src/presentacion/MenuEstudiante.java
@@ -1,13 +1,38 @@
 package presentacion;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MenuEstudiante extends JFrame
 {
 	public MenuEstudiante()
 	{
 		setTitle("Menu Estudiante");
-		setSize(300, 200);
+		setSize(600, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelBotones = new JPanel();
+		panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.LINE_AXIS));
+		
+		JButton botonPerfil = new JButton("Perfil");
+		JButton botonMalla = new JButton("Malla Curricular");
+		JButton botonCertificaciones = new JButton("Inscripción a certificaciones");
+		JButton botonProgreso = new JButton("Seguimiento de Progreso");
+		
+		panelBotones.add(botonPerfil);
+		panelBotones.add(botonMalla);
+		panelBotones.add(botonCertificaciones);
+		panelBotones.add(botonProgreso);
+		
+		add(panelBotones, BorderLayout.NORTH);
+		
+		botonPerfil.addActionListener(e -> {});
+		botonMalla.addActionListener(e -> {});
+		botonCertificaciones.addActionListener(e -> {});
+		botonProgreso.addActionListener(e -> {});
 	}
 }
